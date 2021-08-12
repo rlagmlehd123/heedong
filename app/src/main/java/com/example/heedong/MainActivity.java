4743df2944f60f46d0b8f6ff0d51b9d7957a1503
rlagmlehd123 <rlagmlehd123@naver.com>
2021-08-12 11:45:20 +0900

    first commit

--- app/src/main/java/com/example/heedong/MainActivity.java
+++ app/src/main/java/com/example/heedong/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.heedong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button main_login, main_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main_login = findViewById(R.id.main_login);
+
+        main_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        main_register = findViewById(R.id.main_register);
+        main_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
