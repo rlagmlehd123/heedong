7383db68cd40848794a60affda2d24db1eda1fd3
rlagmlehd123 <rlagmlehd123@naver.com>
2021-08-12 23:51:44 +0900

    2

--- app/src/main/java/com/example/heedong/LoginActivity.java
+++ app/src/main/java/com/example/heedong/LoginActivity.java
@@ -1,14 +1,67 @@
 package com.example.heedong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button login_login, login_register;
+    EditText login_email, login_password;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        login_email = findViewById(R.id.login_email);
+        login_password = findViewById(R.id.login_password);
+
+        login_register = findViewById(R.id.login_register);
+
+        login_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login_login = findViewById(R.id.login_login);
+
+        login_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = login_email.getText().toString().trim();
+                String password = login_password.getText().toString().trim();
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                } else {
+                                    Toast.makeText(LoginActivity.this,"로그인 오류", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/heedong/RegisterActivity.java
+++ app/src/main/java/com/example/heedong/RegisterActivity.java
@@ -1,14 +1,90 @@
 package com.example.heedong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mDatabaseRef;
+    private EditText register_nickname, register_email, register_password, register_passwordcheck;
+    private Button register_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        register_nickname = findViewById(R.id.register_nickname);
+        register_email = findViewById(R.id.register_email);
+        register_password = findViewById(R.id.register_password);
+        register_passwordcheck = findViewById(R.id.register_passwordcheck);
+        register_register = findViewById(R.id.register_register);
+
+        register_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strnickname = register_nickname.getText().toString().trim();
+                String stremail = register_email.getText().toString().trim();
+                String strpassword = register_password.getText().toString().trim();
+                String strpasswordcheck = register_passwordcheck.getText().toString().trim();
+
+                if (strpassword.equals(strpasswordcheck)) {
+                    mFirebaseAuth.createUserWithEmailAndPassword(stremail, strpassword).addOnCompleteListener(
+                            RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                                        String email = user.getEmail();
+                                        String uid = user.getUid();
+                                        String name = register_nickname.getText().toString().trim();
+
+                                        HashMap<Object, String> hashMap = new HashMap<>();
+
+                                        hashMap.put("uid", uid);
+                                        hashMap.put("email", email);
+                                        hashMap.put("name", name);
+
+                                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                        DatabaseReference reference = database.getReference("Users");
+                                        reference.child(uid).setValue(hashMap);
+
+                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                        Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+
+                } else {
+                    Toast.makeText(RegisterActivity.this, "비밀번호가 틀렸습니다.", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
